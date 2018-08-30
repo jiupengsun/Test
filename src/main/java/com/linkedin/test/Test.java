acfc09979c97d83d80849f06166e63b5e3a1559c
Jiupeng Sun <jiusun@jiusun-ld2.linkedin.biz>
2018-08-30 16:54:02 -0700

    test

--- src/main/java/com/linkedin/test/Test.java
+++ src/main/java/com/linkedin/test/Test.java
@@ -1,17 +1,37 @@
 package com.linkedin.test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.stream.Stream;
 
 
 public class Test {
 
-  public static void main(String[] args) {
-    System.out.println(GenericTest.class.equals(GenericTestImpl.class));
+  public static void main(String[] args) throws IOException, URISyntaxException {
+    URI uri = Thread.currentThread().getContextClassLoader().getResource("model").toURI();
+    Path path;
+    if (uri.getScheme().equals("jar")) {
+      FileSystem fileSystem = FileSystems.newFileSystem(uri, Collections.EMPTY_MAP);
+      path = fileSystem.getPath("model");
+      fileSystem.close();
+    } else {
+      path = Paths.get(uri);
+    }
 
-    List<Object> list = new ArrayList<>();
-    list.add(Test.class);
-    System.out.println(list.get(0));
+    //Stream<Path> walk = Files.walk(path, 1);
+    Stream<Path> walk = Files.list(path);
+    for (Iterator<Path> it = walk.iterator(); it.hasNext(); ) {
+      Path p = it.next();
+      System.out.println(p);
+    }
   }
 }
 
